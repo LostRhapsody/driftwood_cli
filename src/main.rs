e786e1c1e87986174b3096f811ddc3a2acb7b52d
Evan Robertson <evan.robertson77@gmail.com>
2024-08-10 16:14:19 -0400

    Added serde_json to the toml to write JSON directly in Rust code, pretty solid Added add site fn in main to call the new netlify add site fn. Also cleaned it up. Netlify: Updated comments and docs Cleaned up existing functions Abstracted building client and sending get and post requests and reading responses new add site fn, can't read a response but it does create a request, name might not work currently.

--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,20 @@ use netlify::{
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // cli::draw_menu();
-    println!("new netlify");
     let netlify: Netlify = Netlify::new("nfp_vc77UcLjcM57aomvo6UsxzJRdRdHNSQie33c");
-    println!("new netlify done");
-    let _ = get_sites(netlify).await;
-    // let _sites = get_sites(netlify);
 
-    // let netlify: Netlify = Netlify::new("nfp_vc77UcLjcM57aomvo6UsxzJRdRdHNSQie33c");
-    // get_site_details(netlify,"56830fd5-ff33-438e-a0fd-2d68868cb2e6");
+    // Get all the sites
+    let _ = get_sites(netlify).await;
+
+    // Create a new site
+    // let _ = add_site(netlify,String::from("awesome-test-site")).await;
+
     Ok(())
 }
 
+/// Get all the sites for the user
+/// netlify: A Netlify instance
+/// Returns a vector of SiteDetails
 async fn get_sites(netlify: Netlify) -> Vec<SiteDetails> {
     match netlify.get_sites().await {
         Ok(sites) => {
@@ -37,11 +40,23 @@ async fn get_sites(netlify: Netlify) -> Vec<SiteDetails> {
     }
 }
 
-async fn get_site_details(netlify: Netlify, id: &str) {
-    Netlify::get_site_details(
-    &netlify,
-    id
-    )
-    .await
-    .expect("Failed to get site details");
+/// Add a new site
+/// netlify: A Netlify instance
+/// site_name: The name of the site to create
+/// Returns a vector of SiteDetails
+async fn add_site(netlify: Netlify, site_name: String) -> Vec<SiteDetails> {
+    match netlify.add_site(site_name).await {
+        Ok(sites) => {
+            println!("Done");
+            for each in &sites {
+                println!("\nSite Details:");
+                println!("{:?}", each);
+            }
+            sites
+        }
+        Err(e) => {
+            println!("Error: {:?}", e);
+            vec![]
+        }
+    }
 }
--- src/netlify.rs
+++ src/netlify.rs
@@ -1,20 +1,26 @@
-/// TODO - Retrieve all the user's sites and display their info in a list
-/// That means the enum will need to be stored in a vector we can iterate over
-/// TODO - Create a new site request
+/// Netlify Module
+/// Used to interact with the Netlify API
+
+/// TODO - Add site request is written but I can't parse the response just yet
 /// TODO - Update a site request
 /// TODO - Shutdown a site request (just stop it, don't delete the whole thing)
 /// TODO - Delete a site request
 /// TODO: Update the Netlify lib so it uses OAuth2 instead of a token
-use reqwest::ClientBuilder;
-
 use serde::Deserialize;
 
+/// Netlify struct
+/// Contains the user agent, token, and base URL for the Netlify API
 pub struct Netlify {
     user_agent: String,
     token: String,
     url: String,
 }
 
+/// SiteDetails struct
+/// Contains the details of a site
+/// name: The name of the site
+/// url: The URL of the site
+/// screenshot_url: The URL of the site's screenshot
 #[derive(Debug, Deserialize)]
 pub struct SiteDetails {
     pub name: Option<String>,
@@ -23,14 +29,16 @@ pub struct SiteDetails {
 }
 
 impl Netlify {
-
     /// Create a struct to store Netlify API connection details
+    /// token: The Netlify API token
+    /// Returns a Netlify struct
     pub fn new(token: &str) -> Netlify {
-        println!("Connecting to Netlify API...");
+        println!("Creating Netlify API Struct...");
+
         // define the user agent
         let user_agent: &str = concat!(
-            env!("CARGO_PKG_NAME"),
-            "/",
+            env!("CARGO_PKG_NAME"), 
+            "/", 
             env!("CARGO_PKG_VERSION"),
         );
 
@@ -41,92 +49,154 @@ impl Netlify {
             user_agent: user_agent.to_string(),
             token: token.to_string(),
             url: base_url,
-        }    
-
+        }
     }
 
-    pub async fn get_site_details(&self, id: &str) -> Result<Vec<SiteDetails>, Box<dyn std::error::Error>>  {
-
-        println!("Getting site info for site id: {}", id);
-    
+    /// Get the details of a site
+    /// id: The ID of the site
+    /// Returns a Result containing a vector of SiteDetails or an error
+    pub async fn get_site_details(
+        &self,
+        id: &str,
+    ) -> Result<Vec<SiteDetails>, Box<dyn std::error::Error>> {
+        // create the url
         let request_url = self.url.clone() + "sites/" + id;
-    
-        // create the builder and client
-        let builder = ClientBuilder::new();                
-        let client = builder
-            .user_agent(&self.user_agent)
-            .build()
-            .unwrap();
+        // build and send the request
+        let client = self.build_client();
+        let response = self.send_get_request(client, request_url).await;
 
-        println!("Client built.");
-
-        let response = client
-            .get(request_url)
-            .bearer_auth(&self.token)
-            .send()
-            .await;
-
-        match response {
-            Ok(resp) => {
-                // println!("Response: {:?}", resp);
-                if resp.status().is_success() {
-                    let sites: Vec<SiteDetails> = resp
-                        .json()
-                        .await?;
-                    // println!("Sites deserialized: {:?}", sites);
-                    return Ok(sites);
-                } else {
-                    println!("Failed to get sites: {}", resp.status());
-                    return Err(format!("Failed to get sites: {}", resp.status()).into());
-                }
-            },
-            Err(e) => {
-                println!("Failed to get sites: {:?}", e);
-                return Err(format!("Failed to get sites: {:?}", e).into());
-            }
-        }
+        self.read_response(response).await
     }
-    
-    pub async fn get_sites(&self) -> Result<Vec<SiteDetails>, Box<dyn std::error::Error>>  {    
-    
-        println!("Connecting to Netlify API...");
+
+    /// Get all the sites for the user
+    /// Returns a Result containing a vector of SiteDetails or an error
+    pub async fn get_sites(
+        &self
+    ) -> Result<Vec<SiteDetails>, Box<dyn std::error::Error>> {
+        // create the url
         let request_url = self.url.clone() + "sites";
-        println!("Request URL: {:?}", request_url);
-    
-        // create the builder and client
-        let builder = ClientBuilder::new();                
-        let client = builder
-            .user_agent(&self.user_agent)
-            .build()
-            .unwrap();
+        // build and send the request
+        let client = self.build_client();
+        let response = self.send_get_request(client, request_url).await;
 
-        println!("Client built.");
+        self.read_response(response).await
+    }
 
+    /// Add a new site
+    /// Returns a Result containing a vector of SiteDetails or an error
+    pub async fn add_site(
+        &self, 
+        site_name: String
+    ) -> Result<Vec<SiteDetails>, Box<dyn std::error::Error>> {
+
+        // create the url
+        let request_url = self.url.clone() + "sites";
+        // create the request body
+        let json = serde_json::json!(
+        {
+            "name": site_name,
+            "ssl": true,
+            "processing_settings": {
+                "html": {
+                    "pretty_urls": true
+                }
+            }
+        });
+        println!("JSON: {}", json.to_string());
+        // build and send the request
+        let client = self.build_client();
+        let response = self.send_post_request(
+            client, 
+            request_url, 
+            json.to_string()
+        ).await;
+        self.read_response(response).await
+    }
+
+    /// Create a reqwest::Client
+    /// Returns a reqwest::Client
+    fn build_client(&self) -> reqwest::Client {
+        println!("Building Client...");
+        let builder = reqwest::ClientBuilder::new();
+        let client = builder.user_agent(&self.user_agent).build().unwrap();
+        client
+    }
+
+    /// Send a request to the Netlify API
+    /// client: The reqwest::Client to use
+    /// request_url: The URL to send the request to
+    /// Returns a Result containing a reqwest::Response or an error
+    async fn send_get_request(
+        &self,
+        client: reqwest::Client,
+        request_url: String,
+    ) -> Result<reqwest::Response, Box<dyn std::error::Error>> {
+        println!("Sending GET request to: {}", request_url);
         let response = client
             .get(request_url)
             .bearer_auth(&self.token)
             .send()
-            .await;
+            .await?;
+        Ok(response)
+    }
+
+    async fn send_post_request(
+        &self,
+        client: reqwest::Client,
+        request_url: String,
+        json: String,
+    ) -> Result<reqwest::Response, Box<dyn std::error::Error>> {
+        println!("Sending POST request to: {}", request_url);        
+        let response = client
+            .post(request_url)
+            .bearer_auth(&self.token)
+            .body(json)
+            .send()
+            .await?;
+        Ok(response)
+    }    
+
+    /// Read the response from the Netlify API
+    /// response: The response from the Netlify API
+    /// Returns a Result containing a vector of SiteDetails or an error
+    async fn read_response(
+        &self,
+        response: Result<reqwest::Response, Box<dyn std::error::Error>>,
+    ) -> Result<Vec<SiteDetails>, Box<dyn std::error::Error>> {
+        println!("Reading Response...");
 
         match response {
             Ok(resp) => {
-                // println!("Response: {:?}", resp);
+                
                 if resp.status().is_success() {
-                    let sites: Vec<SiteDetails> = resp
-                        .json()
-                        .await?;
-                    // println!("Sites deserialized: {:?}", sites);
-                    return Ok(sites);
+
+                    let json: serde_json::Value = resp.json().await?;
+                    println!("JSON Response: {:?}", json);
+
+                    // let sites: Vec<SiteDetails> = resp.json().await?;
+
+                    if let Some(sites) = json.as_array() {
+                        let sites: Vec<SiteDetails> = serde_json::from_value(serde_json::Value::Array(sites.clone()))?;
+                        return Ok(sites);
+                    } else {
+                        return Err("Expected an array of sites".into());
+                    }
+
+                    // return Ok(sites);
                 } else {
                     println!("Failed to get sites: {}", resp.status());
-                    return Err(format!("Failed to get sites: {}", resp.status()).into());
+                    return Err(
+                        format!(
+                            "Failed to get sites: {}", 
+                            resp.status()
+                        ).into()
+                    );
                 }
-            },
+            }
             Err(e) => {
                 println!("Failed to get sites: {:?}", e);
                 return Err(format!("Failed to get sites: {:?}", e).into());
             }
         }
     }
-
 }
