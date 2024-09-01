d4173bba55ede833d4f5ebbb95225c7d12550e29
Evan Robertson <evan.robertson77@gmail.com>
2024-09-01 02:01:16 -0400

    Added generate key, get pub key, and decrypt token functions in the new crypto.rs module. Not implemented for oauth2.0 flow yet.

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,21 @@ pub mod cli;
 pub mod lib;
 pub mod netlify;
 
+pub mod crypto;
 use dotenv::dotenv;
 
 // #[tokio::main]
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
     cli::draw_menu();
+
+    // When initiating OAuth flow:
+    //let (private_key, public_key) = generate_key_pair();
+    //let public_key_pem = get_public_key_pem(&public_key);
+    
+    // When receiving the encrypted token:
+    //let decrypted_token = decrypt_token(&encrypted_token, &private_key);
+
+    // Send public_key_pem to server along with OAuth request
     Ok(())
 }
